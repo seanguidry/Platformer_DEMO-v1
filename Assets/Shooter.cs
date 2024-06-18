57888c40aaed8d456ccf3fb2e51b1d7759c22a3f
seanguidry <2rhys.guid000@proton.me>
2024-06-18 15:06:40 -0500

    New walls who this?

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public Vector3 direction;           //Direction the bullet will go.
     public Transform bulletTransform;   //The Game Object that the Prefab will adhere to.
-    public GameObject bulletPrefab;     //The asset I will use to attach to the Game Object.
-    public float bulletSpeed = 30f;     //The eventual Speed that the bullet will go.
+    public GameObject bulletPrefab;     //The asset I will use to attach to the Game Object.  
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            //setting the direction of the bullet.
-            direction = Vector3.right;
-
-            //Bullet is created when the Spacebar is pressed.
+            //Bullet is created while the Spacebar is pressed.
             var bullet = Instantiate(bulletPrefab, bulletTransform.position, bulletTransform.rotation);
-            
-            //Movement equation from the movement script, altered for use on the bullet's movement.
-            bulletTransform.position = bulletTransform.position + (direction * bulletSpeed * Time.deltaTime);
-            
         }
     }
 }
