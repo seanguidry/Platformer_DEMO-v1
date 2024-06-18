f8cce367f86d00cd2bb4838b7cbe859d3ecc7ea5
seanguidry <2rhys.guid000@proton.me>
2024-06-18 12:56:39 -0500

    Shooter Patch v1,000,000
    Laser time!!!

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public Vector3 direction;
-    public Transform bulletTransform;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 10f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 direction;           //Direction the bullet will go.
+    public Transform bulletTransform;   //The Game Object that the Prefab will adhere to.
+    public GameObject bulletPrefab;     //The asset I will use to attach to the Game Object.
+    public float bulletSpeed = 30f;     //The eventual Speed that the bullet will go.
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKey(KeyCode.Space))
         {
+            //setting the direction of the bullet.
+            direction = Vector3.right;
+
+            //Bullet is created when the Spacebar is pressed.
             var bullet = Instantiate(bulletPrefab, bulletTransform.position, bulletTransform.rotation);
-            Vector3 direction = new Vector3(1, 0);
+            
+            //Movement equation from the movement script, altered for use on the bullet's movement.
+            bulletTransform.position = bulletTransform.position + (direction * bulletSpeed * Time.deltaTime);
+            
         }
-        
-        bulletTransform.position = bulletTransform.position + (direction * bulletSpeed * Time.deltaTime);
     }
 }
   
